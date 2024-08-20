0dd39b823de47eb9201234fd48b76f274c9203d0
Ghostcito <jmguticasani1254@gmail.com>
2024-08-19 19:22:16 -0500

    Primer_Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Proyecto_Personal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
